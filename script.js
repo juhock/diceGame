442e058b33eabbfdf1a9c944272b7dfbd1e34f59
Julia Hocken <ju.hocken@gmail.com>
2024-04-22 13:38:30 -0400

    selected elements for DOM and adjusted dice picture relative path

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Score elements
+const score0Element = document.querySelector("#score--0");
+const score1Element = document.getElementById("score--1");
+const diceElement = document.getElementsByClassName(".dice");
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
