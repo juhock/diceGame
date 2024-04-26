e11dd82dcac40d5b6271c6638231984dbada014e
Julia Hocken <ju.hocken@gmail.com>
2024-04-26 16:27:26 -0400

    add styling for active player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 //Score elements
+const player0Element = document.querySelector(".player--0");
+const player1Element = document.querySelector(".player--1");
 const score0Element = document.querySelector("#score--0");
 const score1Element = document.getElementById("score--1");
 const diceElement = document.querySelector(".dice");
@@ -32,14 +34,16 @@ btnRoll.addEventListener("click", function () {
   diceElement.src = `/media/dice-${dice}.png`;
 
   //check for rolled 1
-
   if (dice !== 1) {
     currentScore += dice;
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
-    current0Element.textContent = currentScore;
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle("player--active");
+    player1Element.classList.toggle("player--active");
   }
 });
