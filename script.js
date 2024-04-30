132546c822ff82d9f11182c879ea12bb072129ff
Julia Hocken <ju.hocken@gmail.com>
2024-04-30 16:22:27 -0400

    change styles and add how to play button

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ btnHold.addEventListener("click", function () {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 10) {
       playing = false;
       diceElement.classList.add("hidden");
 
